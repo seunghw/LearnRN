fbf66ffb9ab0b61b15302ec065ca8ff8514e7b94
Seunghwan <ysp1307@naver.com>
2022-01-14 19:27:38 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+function App(props) {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>gkdlgkdl</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
